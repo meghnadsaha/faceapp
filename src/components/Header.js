80ad0a2fde1aa7b45cfb725b2fe4a906892337de
meghnadsaha <meghnadsaha422@gmail.com>
2024-12-07 00:53:25 +0530

    header fix for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -61,21 +61,36 @@ const Navbar = () => {
                 </ul>
               </li>
 
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown link
                 </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  About
-                </a>
-              </li>
+
+             
             </ul>
           </div>
 
